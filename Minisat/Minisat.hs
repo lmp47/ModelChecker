8998b64060b6b90fed236c8168aab6133b33dc70
lmp47 <lmpick@zoho.com>
2015-11-11 11:54:05 +0000

    Some changes to Minisat interface

--- Minisat/Minisat.hs
+++ Minisat/Minisat.hs
@@ -9,7 +9,6 @@ Haskell bindings for some Minisat functions.
 
 module Minisat.Minisat ( Solver
                        , newSolver
-                       , deleteSolver
                        , addVars
                        , printVars
                        , getVarValue
@@ -22,34 +21,50 @@ import Model.Model
 import Foreign
 import Foreign.C.Types
 import Foreign.C.String
+import Foreign.ForeignPtr
+import System.IO.Unsafe
 
 -----------------------
 -- Wrapper Functions --
 -----------------------
 
--- | Adds variables 0 to n-1 to the solver.
-addVars :: Solver -> Word -> IO ()
-addVars solver n =
-  if n > 0 then do
-    newMinisatVar solver (fromIntegral $ n - 1) 0
-    addVars solver (n - 1)
-  else return ()
+type Solver = IO (ForeignPtr MinisatSolver)
 
--- | Prints variables 0 to n-1 in the solver.
+newSolver :: Solver
+newSolver = newMinisatSolver >>= newForeignPtr deleteMinisatSolver
+
+-- | Adds variables 0 to n - 1 to the solver.
+addVars :: Solver -> Word -> Solver
+addVars solver' n =
+  do
+  solver <- solver'
+  withForeignPtr solver (addVars' n)
+  return solver
+  where
+  addVars' n solver =
+    if n > 0 then do
+      newMinisatVar solver (fromIntegral $ n - 1) 0
+      addVars' (n - 1) solver
+    else return solver
+
+-- | Prints variables 0 to n - 1 in the solver.
 printVars :: Solver -> Word -> IO ()
-printVars solver n =
+printVars solver' n =
   let k = fromIntegral $ n - 1 in
   do
-  int <- valueMinisatVar solver k
+  solver <- solver'
+  int <- withForeignPtr solver (\x -> valueMinisatVar x k)
   print $ "Value of var " ++ (show k) ++ ": " ++ (show $ fromIntegral int)
-  if (k /= 0) then printVars solver (n - 1) else return ()
+  if (k /= 0) then printVars solver' (n - 1) else return ()
 
 -- | Get the value of a variable in the solver.
-getVarValue :: Solver -> Word -> IO (Int)
-getVarValue solver n =
-  do
-  val <- valueMinisatVar solver (fromIntegral n)
-  return $ fromIntegral val
+getVarValue :: Solver -> Word -> Int
+getVarValue solver' n = unsafePerformIO res
+  where
+  res = do
+        solver <- solver'
+        val <- withForeignPtr solver (\x -> valueMinisatVar x (fromIntegral n))
+        return $ fromIntegral val
 
 -- | Add a clause to a veclit
 addToVecLit :: Ptr MinisatVecLit -> [Lit] -> IO ()
@@ -81,50 +96,72 @@ addToVecLit veclit clause =
             addToVecLit veclit vs
 
 -- | Add a clause to a solver.
-addClause :: Solver -> [Lit] -> IO CInt
-addClause solver clause =
+addClause :: Solver -> [Lit] -> Solver
+addClause solver' c =
+  do
+  solver <- solver'
+  withForeignPtr solver (\x -> addClause' x c)
+  return solver
+
+-- | Add a list of clauses to a solver.
+addClauses :: Solver -> ([[Lit]] -> Solver)
+addClauses solver' cs = 
+  do
+  solver <- solver'
+  withForeignPtr solver (\x -> mapM_ (addClause' x) cs)
+  return solver
+
+
+addClause' :: Ptr MinisatSolver -> [Lit] -> IO (Ptr MinisatSolver)
+addClause' solver clause =
   do
   veclit <- newMinisatVecLit
   addToVecLit veclit clause
   addMinisatClause solver veclit
   deleteMinisatVecLit veclit
-  return $ fromIntegral 0
+  return solver
 
--- | Add a list of clauses to a solver.
-addClauses :: Solver -> ([[Lit]] -> IO ())
-addClauses solver = mapM_ (addClause solver)
 
 -- | Have a solver solve with the clauses that have been added.
-solve :: Solver -> IO Bool
-solve solver =
-  do
-  ok <- simplifyMinisat solver
-  if ok == 0
-  then return False
-  else do
-       res <- solveMinisat solver
-       return $ res /= 0
+solve :: Solver -> Bool
+solve solver' = unsafePerformIO res
+  where
+  res =
+    do
+    solver <- solver'
+    ok <- withForeignPtr solver simplifyMinisat
+    if ok == 0
+    then return False
+    else do
+         res <- withForeignPtr solver solveMinisat
+         return $ res /= 0
+
 
 -- | Have a solver solve with the clauses that have been added
 -- along with given assumptions
-solveWithAssumps :: Solver -> [Lit] -> IO Bool
-solveWithAssumps solver assumps =
-  do
-  ok <- simplifyMinisat solver
-  if ok == 0
-  then return False
-  else do
-       veclit <- newMinisatVecLit
-       addToVecLit veclit assumps
-       addMinisatClause solver veclit
-       res <- solveMinisatWithAssumps solver veclit
-       return $ res /= 0
+solveWithAssumps :: Solver -> [Lit] -> Bool
+solveWithAssumps solver' assumps = unsafePerformIO res
+  where
+  res = do
+        solver <- solver'
+        ok <- withForeignPtr solver simplifyMinisat
+        if ok == 0
+        then return False
+        else withForeignPtr solver
+             (\solver ->
+             do
+             veclit <- newMinisatVecLit
+             addToVecLit veclit assumps
+             addMinisatClause solver veclit
+             res <- solveMinisatWithAssumps solver veclit
+             return $ res /= 0)
+
 
 --------------------
 -- Relevant Types --
 --------------------
 
-type Solver = Ptr MinisatSolver
+--type Solver = Ptr MinisatSolver
 
 data MinisatSolver = MinisatSolver
 
@@ -137,31 +174,31 @@ type MinisatVar = CInt
 -----------------
 
 foreign import ccall unsafe "newMinisatSolver"
-  newSolver :: IO Solver
+  newMinisatSolver :: IO (Ptr MinisatSolver)
 
-foreign import ccall unsafe "deleteMinisatSolver"
-  deleteSolver :: Solver -> IO ()
+foreign import ccall unsafe "&deleteMinisatSolver"
+  deleteMinisatSolver :: FinalizerPtr MinisatSolver
 
 foreign import ccall unsafe "newMinisatVar"
-  newMinisatVar :: Solver -> CInt -> CInt -> IO (Ptr MinisatVar)
+  newMinisatVar :: Ptr MinisatSolver -> CInt -> CInt -> IO (Ptr MinisatVar)
 
 foreign import ccall unsafe "releaseMinisatVar"
-  releaseMinisatVar :: Solver -> MinisatVar -> IO ()
+  releaseMinisatVar :: Ptr MinisatSolver -> MinisatVar -> IO ()
 
 foreign import ccall unsafe "addMinisatClause"
-  addMinisatClause :: Solver -> Ptr MinisatVecLit -> IO CInt
+  addMinisatClause :: Ptr MinisatSolver -> Ptr MinisatVecLit -> IO CInt
 
 foreign import ccall unsafe "simplifyMinisat"
-  simplifyMinisat :: Solver -> IO CInt
+  simplifyMinisat :: Ptr MinisatSolver -> IO CInt
 
 foreign import ccall unsafe "solveMinisatWithAssumps"
-  solveMinisatWithAssumps :: Solver -> Ptr MinisatVecLit -> IO CInt
+  solveMinisatWithAssumps :: Ptr MinisatSolver -> Ptr MinisatVecLit -> IO CInt
 
 foreign import ccall unsafe "solveMinisat"
-  solveMinisat :: Solver -> IO CInt
+  solveMinisat :: Ptr MinisatSolver -> IO CInt
 
 foreign import ccall unsafe "valueMinisatVar"
-  valueMinisatVar :: Solver -> CInt -> IO CInt
+  valueMinisatVar :: Ptr MinisatSolver -> CInt -> IO CInt
 
 foreign import ccall unsafe "newMinisatVecLit"
   newMinisatVecLit :: IO (Ptr MinisatVecLit)
@@ -173,18 +210,4 @@ foreign import ccall unsafe "pushMinisatVar"
   pushMinisatVar :: Ptr MinisatVecLit -> MinisatVar -> CInt -> IO ()
 
 foreign import ccall unsafe "printMinisatStats"
-  printMinisatStats :: Solver -> IO ()
-{-
-main :: IO ()
-main =
-  do
-  solver <- newSolver
-  printMinisatStats solver
-  addVars solver 4
-  addClauses solver [[Var 1], [Neg 0], [Var 0, Var 1]]
-  res <- solve solver
-  printMinisatStats solver
-  printVars solver 4
-  print res
-  deleteSolver solver
--}
+  printMinisatStats :: Ptr MinisatSolver -> IO ()
--- Tests/MinisatTester.hs
+++ Tests/MinisatTester.hs
@@ -7,39 +7,13 @@ main :: IO ()
 main =
   do
   -- 0 && not(0)
-  solver <- newSolver
-  addVars solver 2
-  addClauses solver [[Var 0], [Neg 0]]
-  res <- solve solver
-  deleteSolver solver
-  if res then error "0 && not(0) : SAT" else return ()
+  print $ show $ solve $ addClauses (addVars newSolver 2) [[Var 0], [Neg 0]]
   -- (0 || 1) && (0 || not(1))
-  solver <- newSolver
-  addVars solver 4
-  addClauses solver [[Var 0, Var 1], [Var 0, Neg 1]]
-  res <- solve solver
-  if (not res)
-    then error "(0 || 1) && (0 || not(1)) : UNSAT"
-    else return ()
-  deleteSolver solver
+  print $ show $ solve $ addClauses (addVars newSolver 4) [[Var 0, Var 1], [Var 0, Neg 1]]
   -- 0 && (2 || 3) && not(3)
-  solver <- newSolver
-  addVars solver 7
-  addClauses solver [[Var 0], [Var 2, Var 3], [Neg 3]]
-  res <- solve solver
-  if (not res)
-    then error "0 && (2 || 3) && not(3) : UNSAT"
-    else return ()
-  true <- getVarValue solver 2
-  if (true == 0)
-    then error "0 && (2 || 3) && not(3) : SAT with Neg 2"
-    else return ()
-  deleteSolver solver
-  -- Solve with assumptions: not(0), assuming 0
-  solver <- newSolver
-  addVars solver 2
-  addClauses solver [[Neg 0], [Neg 1]]
-  res <- solveWithAssumps solver [Var 0, Var 1]
-  if res then error "0 && 1 => not(0) && not(1) : SAT" else return ()
+  print $ show $ solve $ addClauses (addVars newSolver 7) [[Var 0], [Var 2, Var 3], [Neg 3]]
+  -- Solve with assumptions: not(0) && not(1), assuming 0 && 1
+  print $ show $ solveWithAssumps (addClauses (addVars newSolver 4) [[Neg 0], [Neg 1]]) [Var 0, Var 1]
+  -- Solve with assumptions: (not(0) || 0) && not(1), assuming not(0)
+  print $ show $ solveWithAssumps (addClauses (addVars newSolver 4) [[Neg 0, Var 0], [Neg 1]]) [Neg 0]
   print "Done."
-  
